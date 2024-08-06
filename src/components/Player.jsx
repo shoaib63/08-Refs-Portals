c2c2da987dfe7b041db0f31e46b05b2003a433b9
shoaib63 <mirakishoaib@gmail.com>
2024-08-06 20:16:29 +0330

    End Of RefsPortal

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState();
+  const playerRef = useRef();
+
+  function HandleClick() {
+    setPlayerName(playerRef.current.value);
+  }
+
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? 'unknown entity'} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerRef} type="text" />
+        <button onClick={HandleClick}>Set Name</button>
       </p>
     </section>
   );
